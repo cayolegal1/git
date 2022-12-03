992dfcb3bebe136d289bab5ab13f574b56669a8f
Cayo <legalfigueredoc@gmail.com>
2022-12-03 19:41:06 -0300

    stash and reset --soft --mixed concepts

--- test.js
+++
@@ -1,7 +0,0 @@
-Array.prototype.saludar = function(){
-    return this.forEach(item => typeof item === 'string' && console.log(`Hola ${item}`))
-}
-
-const arr = ['Julio', 'Maria', 'Mama', {nombre: 'hola'}]
-
-arr.saludar()
